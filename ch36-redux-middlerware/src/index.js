8fd375f6153a84f63c75759d00d4a6f9126bd1e3
한희옥 <fortrulymylife@gmail.com>
2021-05-13 05:21:39 +0900

    ch36: 04 add redux-thunk

--- ch36-redux-middlerware/src/index.js
+++ ch36-redux-middlerware/src/index.js
@@ -8,10 +8,11 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './modules';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
 );
 
 ReactDOM.render(
