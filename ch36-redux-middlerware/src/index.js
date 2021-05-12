914c922b7ef2d0ff7238db4329e6b64bbdae5e7b
한희옥 <fortrulymylife@gmail.com>
2021-05-13 05:17:28 +0900

    ch36: 03  add logger, devTool

--- ch36-redux-middlerware/src/index.js
+++ ch36-redux-middlerware/src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './modules';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
