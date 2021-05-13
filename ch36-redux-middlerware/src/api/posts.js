b0ceb5dfe4b570874b70cbc1e682c38daccab049
한희옥 <fortrulymylife@gmail.com>
2021-05-13 10:31:03 +0900

    ch36: 15 use json-sever

--- ch36-redux-middlerware/src/api/posts.js
+++ ch36-redux-middlerware/src/api/posts.js
@@ -1,29 +1,11 @@
-const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
-
-const posts = [
-  {
-    id: 1,
-    title: '리액트는 안드로메다로',
-    body: '열심히 해보자. 해야 는당',
-  },
-  {
-    id: 2,
-    title: 'redux-thunk',
-    body: 'redux thunk 를 배워서 남주고 redux-saga 를 사용하자',
-  },
-  {
-    id: 3,
-    title: 'redux-saga',
-    body: 'redux-saga 는 배워서 redux-toolkit과 같이 사용하자.',
-  },
-];
+import axios from 'axios';
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const res = await axios.get('http://localhost:4000/posts');
+  return res.data;
 };
 
 export const getPostById = async (id) => {
-  await sleep(500);
-  return posts.find((post) => post.id === id);
+  const res = await axios.get(`http://localhost:4000/posts/${id}`);
+  return res.data;
 };
